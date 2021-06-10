d640ef42dfcb199c2945ad3512c950b2d9cdb234
waynee95 <larsericmarquardt@mail.de>
2021-06-10 23:11:03 +0200

    Add AST

---
+++ src/ast/ast.ml
@@ -0,0 +1,58 @@
+type id = string
+
+(* Rift basic data types *)
+type typ = TInt | TString | TBool | TArray of typ | TCustom of string
+
+type field = id * typ
+
+type binop =
+  | Plus
+  | Minus
+  | Mult
+  | Div
+  | Rem
+  | LessThan
+  | LessEqual
+  | GreaterThan
+  | GreaterEqual
+  | And
+  | Or
+  | Eq
+  | NotEq
+
+type unop = Not | Neg
+
+type exp =
+  (* Literals *)
+  | IntLit of int
+  | StringLit of string
+  | BoolLit of bool
+  (* Array and Record construction *)
+  | Array of exp list
+  | Location of location
+  | Record of id * exp list
+  | FuncCall of {name: id; args: exp list}
+  | BinOp of exp * binop * exp
+  | UnOp of unop * exp
+  | Assign of location * exp
+  | If of {cond: exp; true': exp; false': exp}
+  | While of exp * exp
+  | Break
+  | Let of {decls: decl list; body: exp}
+  | Seq of exp list
+(* TODO: Match *)
+
+and location =
+  | Id of id
+  | FieldAccess of location * id
+  | Index of location * exp
+
+and decl =
+  | RecordDecl of id * field list
+  (* TODO: EnumDecl *)
+  | VarDecl of {name: id; typ: typ option; value: exp; vartyp: vartyp}
+  | FuncDecl of {name: id; params: field list; returnTyp: typ option; body: exp}
+  | ExternDecl of {name: id; params: field list; returnTyp: typ option}
+
+(* Determines whether a variable is immutable=val or mutable=var *)
+and vartyp = Var | Val
