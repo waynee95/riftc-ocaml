a4d1eb9d7afe22543c1cfeecd63fbd0da028c624
waynee95 <larsericmarquardt@mail.de>
2021-07-01 12:03:53 +0200

    Add command-line args handling

--- src/main.ml
+++ src/main.ml
@@ -2,5 +2,14 @@ open Parsing
 
 let parse s =
   let lexbuf = Lexing.from_string s in
-  let ast = Parser.prog Lexer.read lexbuf in
-  ast
+  Parser.prog Lexer.read lexbuf
+
+let print_parse _ = failwith "TODO: unimplemented"
+
+let options = [("-p", Arg.String print_parse, "Parse input and print AST")]
+
+let unrecognised x = "unrecognised option " ^ x ^ ", use -help" |> print_endline
+
+let main () = Arg.parse options unrecognised "Usage: riftc [options] FILE"
+
+let _ = main ()
